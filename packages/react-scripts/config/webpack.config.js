6d09aa63b9f118e1ce0d660376b12ee599f47093
Lars-Jørgen Kristiansen <me@larsjk.com>
2022-01-24 14:09:05 +0100

    Allow imports from ../common/js

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -334,7 +334,7 @@ module.exports = function (webpackEnv) {
         // To fix this, we prevent you from importing files out of src/ -- if you'd like to,
         // please link the files into your node_modules/ and let module-resolution kick in.
         // Make sure your source files are compiled, as they will not be processed in any way.
-        new ModuleScopePlugin(paths.appSrc, [
+        new ModuleScopePlugin([paths.appSrc, paths.commonJs], [
           paths.appPackageJson,
           reactRefreshRuntimeEntry,
           reactRefreshWebpackPluginRuntimeEntry,
@@ -413,7 +413,7 @@ module.exports = function (webpackEnv) {
             // The preset includes JSX, Flow, TypeScript, and some ESnext features.
             {
               test: /\.(js|mjs|jsx|ts|tsx)$/,
-              include: paths.appSrc,
+              include: [paths.appSrc, paths.commonJs],
               loader: require.resolve('babel-loader'),
               options: {
                 customize: require.resolve(
